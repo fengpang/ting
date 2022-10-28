5c48218d46dcd3e0207f7a331578cc06cd500a1a
yyc <928043359@qq.com>
2022-10-28 10:37:38 +0800

    fix: add mergeDefaultOptions

--- src/store/config.ts
+++ src/store/config.ts
@@ -9,8 +9,8 @@ export const useConfigStore = defineStore('config', {
       { name: '早起', icon: 'candy', finishedCount: 0, maxCount: 1, starCount: 1, status: 0 },
       { name: '锻炼', icon: 'candy', finishedCount: 0, maxCount: 1, starCount: 2, status: 0 },
       { name: '口语', icon: 'candy', finishedCount: 0, maxCount: 1, starCount: 2, status: 0 },
-      { name: '听力', icon: 'candy', finishedCount: 0, maxCount: 8, starCount: 6, status: 0 },
-    ])
+      { name: '听力', icon: 'candy', finishedCount: 0, maxCount: 8, starCount: 8, status: 0 },
+    ], localStorage, { mergeDefaults: true })
     const rewardList = useStorage('rewardList', [
       { name: '金豪b椅子', price: 2400, status: 0 },
     ])
